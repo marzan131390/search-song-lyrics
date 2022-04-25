2551dc38892b8d0054ffd97bab20563e8e18858a
marzan131390 <marzandwmtec@gmail.com>
2022-04-25 17:25:42 +0600

    all lyrics display is done

--- script/script.js
+++ script/script.js
@@ -21,13 +21,53 @@ searchBtn.addEventListener("click", function() {
         let searchText = lyricsInput.value;
         fetch(`https://api.lyrics.ovh/suggest/${searchText}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            let allData = data.data.slice(0, 4);
+           for(let i = 0; i<allData.length; i++) {
+               let singleData = allData[i];
+            //    console.log(singleData);
+               displayDataValue(singleData);
+           };
+        });
     }
 })
 //Remove Warning when input field is clicked.
 lyricsInput.addEventListener("click", function() {
     warning.style.display = "none";
 })
+//Display data value..
+let contentElement = document.getElementById("content");
+function displayDataValue(value) {
+    let allSongDetails = document.createElement("div");
+    allSongDetails.classList.add("all-song-details")
+    allSongDetails.innerHTML = `
+        <div class="album-content"> 
+            <h1>Artist Name: ${value.artist.name}</h1>
+            <p>Song Title: ${value.title}</p>
+            <audio controls>
+                <source src="${value.preview}">
+            </audio>
+        </div>
+        <div> 
+            <button class="get-lyrics custom-btn" onclick="displayLyrics('${value.artist.name}', '${value.title}')">Get Lyrics</button>
+        </div>
+    `;
+    contentElement.appendChild(allSongDetails);
+};
+//Display Lyrics..
+let lyrics = document.querySelector(".lyrics");
+function displayLyrics(artist, title) {
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then(response => response.json())
+    .then(data => {
+        lyrics.innerText = `${data.lyrics}`
+        console.log(data.lyrics);
+        console.log(lyrics);
+
+    })
+    
+}
+
 
 // https://api.lyrics.ovh/suggest/${searchText}
 // https://api.lyrics.ovh/v1/artist/title
